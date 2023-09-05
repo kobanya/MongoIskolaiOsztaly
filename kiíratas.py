040b467cb8ef7a09692b0165678e0df00111ee8b
bela <nagy.bela.budapest@gmail.com>
2023-09-05 19:41:56 +0200

    átlag

--- kiíratas.py
+++ kiíratas.py
@@ -24,6 +24,13 @@ def main():
 
         if fizika_jegyek:
             print("Nagy Béla Fizika érdemjegyei:", ", ".join(map(str, fizika_jegyek)))
+
+            # Érdemjegyek átlagának kiszámítása
+            if len(fizika_jegyek) > 0:
+                atlag = sum(fizika_jegyek) / len(fizika_jegyek)
+                print(f"Jegyek átlaga: {atlag:.1f}")
+            else:
+                print("Nincs érdemjegy az átlaghoz.")
         else:
             print("Nagy Béla Fizika érdemjegyei nem találhatóak.")
     else:
