7342d6cc531464d0efb930ce2b858f871ee2e3c7
bela <nagy.bela.budapest@gmail.com>
2023-09-05 18:00:10 +0200

    Kiegészítve kiíratással

--- diakok_tantargyak.py
+++ diakok_tantargyak.py
@@ -3,18 +3,21 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017/")
 db = client["ISKOLA"]
 
+diakok_collection = db["Diakok"]
+tantargyak_collection = db["Tantargyak"]
+erdemjegyek_collection = db["Erdemjegyek"]
+
 if "Diakok" in db.list_collection_names() and "Tantargyak" in db.list_collection_names():
     print("Az adatbázis már létezik!")
 else:
-    diakok_collection = db["Diakok"]
-    tantargyak_collection = db["Tantargyak"]
-    erdemjegyek_collection = db["Erdemjegyek"]
-
     diakok_collection.insert_many([
         {"nev": "Kiss Brigitta"},
         {"nev": "Nagy Béla"},
         {"nev": "Ferenczi Szabolcs"},
         {"nev": "Klein Noel"},
+        {"nev": "Podhorszky Szilvia"},
+        {"nev": "Matus Zsolt"},
+        {"nev": "Harmati Vivien"},
         {"nev": "Láng Tamás"}
     ])
 
@@ -38,3 +41,11 @@ else:
 
     print("Érdemjegyek hozzáadva.")
 
+# Kiíratás
+print("Diák\t\t\t\t\tÉrdemjegyek")
+print("------------------------------------------------------")
+for diak in diakok_collection.find():
+    diak_nev = diak["nev"]
+    erdemjegyek = erdemjegyek_collection.find({"diak_id": diak["_id"]})
+    erdemjegyek_text = "\t".join([str(t["erdemjegy"]) for t in erdemjegyek])
+    print(f"{diak_nev:<30}\t{erdemjegyek_text}")
